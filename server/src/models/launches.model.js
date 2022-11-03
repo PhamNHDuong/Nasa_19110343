1ef5ac017fc00c1bfe4a97f1d8b3be6fd918eea4
catb0t0 <66787728+catb0t0@users.noreply.github.com>
2022-11-03 19:07:12 +0700

    first commit

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,50 @@
+const launches = new Map();
+
+let latestFlightNumber = 100;
+
+const launch = {
+    flightNumber: 100,
+    mission: 'Kepler Exploration X',
+    rocket: 'Explorer IS1',
+    launchDate: new Date('December 27, 2030'),
+    target: 'Kepler-442 b',
+    customer: ['ZTM', 'NASA'],
+    upcoming: true,
+    success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+    return launches.has(launchId)
+}
+
+function getAllLaunches(){
+    return Array.from(launches.values());
+}
+
+function addNewLaunches(launch){
+    latestFlightNumber++;
+    launches.set(
+        launch.flightNumber, 
+        Object.assign(launch, {
+            success: true,
+            upcoming: true,
+            customer: ['Zero To Mastery', 'NASA'],
+            flightNumber: latestFlightNumber,
+    }));
+}
+
+function abortLaunchById(launchId){
+    const aborted = launch.delete(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+    return aborted;
+}
+
+module.exports = {
+    getAllLaunches,
+    addNewLaunches,
+    existsLaunchWithId,
+    abortLaunchById,
+}
